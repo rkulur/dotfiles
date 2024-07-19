46142064ef41fd94ac5c61fbf34e561aaabbc68c
RKulur <rashwithkulur07@gmail.com>
2024-07-19 15:15:10 +0530

    Initial commit

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "horizontal",
+      open_mapping = [[<C-\>]],
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = "1",
+      start_in_insert = true,
+      persist_size = true,
+    },
+    config = true,
+  },
+}
