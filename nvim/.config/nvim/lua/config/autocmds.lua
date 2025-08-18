9bf45761f1af0f99993c68442788412d45ffaf80
rkulur <rashwithkulur07@gmail.com>
2025-08-18 18:48:52 +0530

    Make nvim think cwd as root directory

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Fix command-line window height
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  callback = function()
+    vim.cmd("resize 1") -- set height to 1 lines
+  end,
+})
